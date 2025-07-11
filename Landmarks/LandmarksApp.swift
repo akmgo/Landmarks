ee7a8a13a79e32de6ef675b5c545943c1ea1cf75
akram_alex <akmgo2024@outlook.com>
2025-07-11 22:27:55 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by akram on 2025/7/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
