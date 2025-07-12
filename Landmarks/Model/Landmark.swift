7df103d1d6435d6184d8c3977a1e0987446f8081
akram_alex <akmgo2024@outlook.com>
2025-07-12 20:27:57 +0800

    make the list dynamic

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -5,11 +5,11 @@
 //  Created by akram on 2025/7/12.
 //
 
+import CoreLocation
 import Foundation
 import SwiftUI
-import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
@@ -20,19 +20,18 @@ struct Landmark: Hashable, Codable {
     var image: Image {
         Image(imageName)
     }
-    
-    private var coordinates:Coordinates
-    var locationCoordinate:CLLocationCoordinate2D{
+
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude
         )
     }
-    
-    struct Coordinates:Hashable,Codable{
-        var latitude:Double
-        var longitude:Double
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
     }
-    
-    
+
 }
