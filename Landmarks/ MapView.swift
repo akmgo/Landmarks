59ae9a1972026f74d6e1da515fb4cf109d936f17
akram_alex <akmgo2024@outlook.com>
2025-07-12 12:34:34 +0800

    map view

---
+++ Landmarks/ MapView.swift
@@ -0,0 +1,18 @@
+//
+//   MapView.swift
+//  Landmarks
+//
+//  Created by akram on 2025/7/12.
+//
+
+import SwiftUI
+
+struct _MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _MapView()
+}
